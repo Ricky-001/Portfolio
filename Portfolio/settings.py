bcd10617b1e9c7c57502ce1ad50be7a913cb04b5
Ricky-001 <raktim.saha.006@gmail.com>
2021-07-22 15:57:17 +0530

    Initial stage - PC supported

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5l$o&(u#@r0htdh%rb(l(^hyier+a6nn-8b0y#jq&hylwebg64'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
